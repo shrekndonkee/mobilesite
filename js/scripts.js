f6b983628a6b313854b5a2a6e395ba3dc695c83d
Alan Martinez-Diaz <alanmartindiaz21@gmail.com>
2024-11-28 21:46:53 -0700

    done

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+const btnElement = document.querySelector('#primaryNav > button');
+const navElement = document.querySelector('#primaryNav > ul');
+
+
+btnElement.addEventListener('click', () => {
+btnElement.classList.toggle('open');
+navElement.classList.toggle('open');
+})
